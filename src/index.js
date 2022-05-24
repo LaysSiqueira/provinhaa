458156354e5dca39c9591d5b7be5badc92ca3e03
LaysSiqueira <siqueiralays2004@gmail.com>
2022-05-24 09:37:38 -0300

    provinhaa

--- src/index.js
+++ src/index.js
@@ -1,20 +1,45 @@
 import 'dotenv/config'
 import express from 'express'
 import cors from 'cors'
+import { soma,media,dobro } from './servico.js'
+
+
 
 const server = express();
 server.use(cors());
- 
-server.listen(process.env.PORT,
-               ()=> console.log('api online na porta ${process.env.PORT}'));
+server.use(express.json());
 
 server.get ('/ping',(req, resp)=>{
     resp.send('pong');
 })               
 
-server.get('dobro/:numero',(req, resp)=>{
-    let numero=Number(req.params.numero);
-    let dobro=numero*2;
-    resp.send(String(dobro));
-    
+server.get('dobro/:numero' , (req, resp)=> {
+    let nu= Number(req.params.numero);
+    let resposta = dobro(nu)
+
+    resp.send({
+        dobro:resposta
+    })
 })
+
+server.listen(process.env.PORT,
+    ()=> console.log(`api online na porta ${process.env.PORT}`));
+
+server.get('/somar',(req,resp)=>{
+    let {a,b}= req.query;
+    let x = soma(Number(a),Number(b))
+
+    resp.send({
+        soma: x
+    })
+})
+
+server.get('/media',(req,resp)=>{
+    let {n1, n2, n3} = req.query;
+    let y = media(Number(n1), Number(n2), Number(n3));
+    resp.send({
+        media: y
+    })
+})
+
+
---
+++ src/servico.js
@@ -0,0 +1,15 @@
+export function soma(a,b){
+    let conta=a+b
+    return conta
+}
+
+export function media (n1,n2,n3){
+    let x = ( n1 + n2 + n3) / 3; 
+    return x
+}
+
+export function dobro(numero){
+    let x= numero*2;
+
+    return x
+}
